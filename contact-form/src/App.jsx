1cba45cf98a3b559a0c07b73d30e70f75084ec58
TankedThomas <TankedThomas@users.noreply.github.com>
2024-03-17 08:23:31 +1300

    Migrate contact form to Vite

---
+++ contact-form/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contact from './components/Contact';
+
+// Render Site
+function App() {
+  return (
+    <div>
+      <Contact></Contact>
+    </div>
+  );
+}
+
+export default App
